19fd055ad67d07df1f3e3315c0eb1bc352b7c012
Jon Hallam <Jon@decisions.com>
2020-08-18 10:17:46 +0100

    Initial commit

---
+++ dump/SnowFlakeRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using DecisionsFramework.Design.Flow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnowFlakeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sut = new Snowflake.SnowflakeIntegration();
+
+            var result = sut.QuerySnowFlake("select * from json_weather_data_view where date_trunc('month',observation_time) = '2018-01-01' limit 10", "https://jba31166.us-east-1.snowflakecomputing.com",
+                "jba31166", "jonhallam", "bownem-wyXvix-3kovwy", "Weather", "public");
+        }
+    }
+}
